3f6e8da9f50dd94ad6f7900f492afdea32998c50
Your Name <you@example.com>
2022-07-23 13:59:43 -0600

    redid code to simplify as well as have the battle page info inserted

--- js/app.js
+++ js/app.js
@@ -1,66 +1,62 @@
-function fight_one(details){
-    if (Cookies.set(`Pokemon`,`Chikorita` )) {
-        location.href = `fight.html`   
-    }
+function fight_one(details) {
+    let pick_chikorita_json = JSON.stringify(pokemon_chikorita)
+    Cookies.set(`Pokemon`,pick_chikorita_json )
+    Cookies.set(`user_health`,`${pokemon_chikorita[`health`]}` )
+    location.href = `fight.html`
 }
 
-
-let chikorita = document.getElementById(`chikorita_button`);
-chikorita.addEventListener(`click`, fight_one)
-
-
-function fight_two (details) {
-    if (Cookies.set(`Pokemon`, `Cyndaquil` )) {
-        location.href = `fight.html` 
-    }
+function fight_two(details) {
+    let pick_cyndaquil_json = JSON.stringify(pokemon_cyndaquil)
+    Cookies.set(`Pokemon`,pick_cyndaquil_json )
+    Cookies.set(`user_health`,`${pokemon_cyndaquil[`health`]}` )
+    location.href = `fight.html`
 }
 
-
-let cyndaquil = document.getElementById(`cyndaquil_button`);
-cyndaquil.addEventListener(`click`, fight_two)
-
-
-
-function fight_three (details) {
-    if (Cookies.set(`Pokemon`, `Totadile` )) {
-        location.href = `fight.html` 
-    }
+function fight_three(details) {
+    let pick_totodile_json = JSON.stringify(pokemon_totodile)
+    Cookies.set(`Pokemon`,pick_totodile_json )
+    Cookies.set(`user_health`,`${pokemon_totodile[`health`]}` )
+    location.href = `fight.html`
 }
 
+let pokemon_chikorita = 
 
-let totadile = document.getElementById(`totadile_button`);
-totadile.addEventListener(`click`, fight_three)
+    {
+        name: `Chikorita`,
+        image_url: `/chiko_idle.gif`,
+        health: 50,
+        attacks: `tackle`
+    };
+
+    let pokemon_cyndaquil =
+    {
+
+        name: `Cyndaquil`,
+        image_url: `/cyndaquil.gif`,
+        health: 55,
+        attacks: `tackle`
+    };
+
+    let pokemon_totodile =
+    {
+
+        name: `Totadile`,
+        image_url: `/totadile.gif`,
+        health: 60,
+        attacks: `tackle`
+    };
 
 
 
 
 
+let chikorita_click = document.getElementById(`chikorita_button`);
+chikorita_click.addEventListener(`click`, fight_one)
 
-let pokemon_selection =[
 
-{
-    name: `Chikorita`,
-    image_url: `/chiko_idle.gif`,
-    health: `50`,
-    attacks:`tackle`
-},
+let cyndaquil_click = document.getElementById(`cyndaquil_button`);
+cyndaquil_click.addEventListener(`click`, fight_two)
 
-{
+let totodile_click = document.getElementById(`totadile_button`);
+totodile_click.addEventListener(`click`, fight_three)
 
-    name: `Cyndaquil`,
-    image_url: `/cyndaquil.gif`,
-    health: `50`,
-    attacks:`tackle` 
-},
-
-{
-
-    name: `Totadile`,
-    image_url: `/totadile.gif`,
-    health: `50`,
-    attacks:`tackle`
-}
-]
-
-let pokemon_selection_Json = JSON.stringify(pokemon_selection);
-Cookies.set(`user_selection`, pokemon_selection_Json);
--- js/fight.js
+++ js/fight.js
@@ -1,24 +1,18 @@
-// let selection = Cookies.get(`Pokemon`)
-// if (selection === `Chikorita`) {
-//     let fight_header = document.getElementById(`fight_header`);
-//     fight_header.insertAdjacentHTML(`beforeend`, `<section>
-//     <h1></h1>
+let select_Json = Cookies.get(`Pokemon`);
+let selection_Data = JSON.parse(select_Json);
 
-//     </section>`)
-
-// }
-
-
-let pokemon_select_Json = Cookies.get(`user_selection`);
-if (pokemon_select_Json === undefined) {
-    console.log(`error`)
-}
-let selection_Data = JSON.parse(pokemon_select_Json);
 document.body.insertAdjacentHTML(`beforeend`,
 
     `<section>
     <h1>${selection_Data[`name`]}</h1>
     <img src="${selection_Data[`image_url`]}" >
     <p>${selection_Data[`health`]}</p>
+    <button>${selection_Data[`attacks`]}</button>
+
   
     </section>`);
+
+
+
+
+
