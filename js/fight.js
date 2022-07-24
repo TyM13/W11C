c155ba3e5f59b95f69d1d2ae0c8a18b7d0d245c2
Your Name <you@example.com>
2022-07-24 00:40:27 -0600

    added a conditional to print if you win or lose

--- js/fight.js
+++ js/fight.js
@@ -3,12 +3,13 @@ let selection_Data = JSON.parse(select_Json);
 let user_health = Cookies.get(`user_health`)
 let comp_health = Cookies.get(`comp_health`)
 
+
 document.body.insertAdjacentHTML(`beforeend`,
 
     `<section>
     <h1>${selection_Data[`name`]}</h1>
     <img src="${selection_Data[`image_url`]}" >
-    <p>${user_health}</p>
+    <p id="current_user_hp">${user_health}</p>
     <button id="tackle_attack">${selection_Data[`attacks`]}</button>
   
     </section>`);
@@ -23,22 +24,30 @@ document.body.insertAdjacentHTML("beforebegin",
 `<section>
 <h1>${comp_selection_Data[`name`]}</h1>
 <img src="${comp_selection_Data[`image_url`]}" >
-<p>${comp_health}</p>
+<p id="current_comp_hp">${comp_health}</p>
 </section>`);
 
 
 
-
+let comp_hp = document.getElementById(`current_comp_hp`);
+let user_hp = document.getElementById(`current_user_hp`);
 
 
 function pokemon_fight (details) {
 comp_health = comp_health -10
 Cookies.set(`comp_health`, comp_health)
-    
-user_health = user_health -5
+comp_hp[`innerHTML`] = (`comp_health`, comp_health)
+ user_health = user_health -5
 Cookies.set(`user_health`, user_health)
-}
+user_hp[`innerHTML`] = (`user_health`, user_health) 
 
+
+if (comp_health <= 0) {
+  return document.body.insertAdjacentHTML("afterend",`<h1>You Win</h1>`);
+     }else if(user_health <= 0) {
+        document.body.insertAdjacentHTML("afterend",`<h1>You Fainted</h1>`);
+    }   
+}
 
 let tackle_attk = document.getElementById(`tackle_attack`);
 tackle_attk.addEventListener(`click`, pokemon_fight)
