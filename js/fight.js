5cbeb64cadc9505258f95c1c63d1931d9c8abfde
Your Name <you@example.com>
2022-07-24 18:00:19 -0600

    moved where you win/fainted is placed

--- js/fight.js
+++ js/fight.js
@@ -6,9 +6,9 @@ let comp_health = Cookies.get(`comp_health`)
 
 document.body.insertAdjacentHTML(`beforeend`,
 
-    `<section>
+    `<section id="comp_section">
     <h1>${selection_Data[`name`]}</h1>
-    <img src="${selection_Data[`image_url`]}" >
+    <img class="pokemon_imgs"src="${selection_Data[`image_url`]}" >
     <p id="current_user_hp">${user_health}</p>
     <button id="tackle_attack">${selection_Data[`attacks`]}</button>
   
@@ -19,11 +19,11 @@ document.body.insertAdjacentHTML(`beforeend`,
 let comp_select_Json = Cookies.get(`comp_Pokemon`);
 let comp_selection_Data = JSON.parse(comp_select_Json);
 
-document.body.insertAdjacentHTML("beforebegin",
+document.body.insertAdjacentHTML("beforeend",
 
 `<section>
 <h1>${comp_selection_Data[`name`]}</h1>
-<img src="${comp_selection_Data[`image_url`]}" >
+<img class="pokemon_imgs" src="${comp_selection_Data[`image_url`]}" >
 <p id="current_comp_hp">${comp_health}</p>
 </section>`);
 
@@ -41,11 +41,20 @@ comp_hp[`innerHTML`] = (`comp_health`, comp_health)
 Cookies.set(`user_health`, user_health)
 user_hp[`innerHTML`] = (`user_health`, user_health) 
 
+let comp_section = document.getElementById(`comp_section`)
 
 if (comp_health <= 0) {
-  return document.body.insertAdjacentHTML("afterend",`<h1>You Win</h1>`);
+    tackle_attk[`style`][`display`] = `none`
+    comp_section.insertAdjacentHTML("beforeend",`<h1>You Win</h1>`); //
+ return comp_hp[`innerHTML`] = (``)
+
+   
      }else if(user_health <= 0) {
-        document.body.insertAdjacentHTML("afterend",`<h1>You Fainted</h1>`);
+        tackle_attk[`style`][`display`] = `none`
+        comp_section.insertAdjacentHTML("beforeend",`<h1 class="win_lose">You Fainted</h1>`); //
+     return  user_hp[`innerHTML`] = (``) 
+
+        
     }   
 }
 
