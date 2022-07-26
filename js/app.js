d63cb1ad053cacc3eae8949daa51cfd0d7c6128a
Your Name <you@example.com>
2022-07-26 00:08:01 -0600

    added comments to assignment

--- js/app.js
+++ js/app.js
@@ -1,26 +1,26 @@
 function fight_one(details) {
-    let pick_chikorita_json = JSON.stringify(pokemon_chikorita)
-    Cookies.set(`Pokemon`,pick_chikorita_json )
-    Cookies.set(`user_health`,`${pokemon_chikorita[`health`]}` )
-    location.href = `fight.html`
+    let pick_chikorita_json = JSON.stringify(pokemon_chikorita)     // creates variable and stores pokemon_chikorita (object) in it using json.stringify
+    Cookies.set(`Pokemon`,pick_chikorita_json )                     // sets a cookie name as pokemon and value as pick_chikorita_json
+    Cookies.set(`user_health`,`${pokemon_chikorita[`health`]}` )    //sets a cookie name as user_health and goes into the object pokemon_chikorita  and sets the value as the key health
+    location.href = `fight.html`                                    // brings you to fight page
 }
 
 function fight_two(details) {
-    let pick_cyndaquil_json = JSON.stringify(pokemon_cyndaquil)
-    Cookies.set(`Pokemon`,pick_cyndaquil_json )
-    Cookies.set(`user_health`,`${pokemon_cyndaquil[`health`]}` )
-    location.href = `fight.html`
+    let pick_cyndaquil_json = JSON.stringify(pokemon_cyndaquil)      // creates variable and stores pokemon_cyndaquil (object) in it using json.stringify
+    Cookies.set(`Pokemon`,pick_cyndaquil_json )                      // sets a cookie name as pokemon and value as pick_cyndaquil_json
+    Cookies.set(`user_health`,`${pokemon_cyndaquil[`health`]}` )     //sets a cookie name as user_health and goes into the object pokemon_cyndaquil health and sets the value as health
+    location.href = `fight.html`                                     // brings you to fight page
 }
 
 function fight_three(details) {
-    let pick_totodile_json = JSON.stringify(pokemon_totodile)
-    Cookies.set(`Pokemon`,pick_totodile_json )
-    Cookies.set(`user_health`,`${pokemon_totodile[`health`]}` )
-    location.href = `fight.html`
+    let pick_totodile_json = JSON.stringify(pokemon_totodile)        // creates variable and stores pokemon_totodile (object) in it using json.stringify
+    Cookies.set(`Pokemon`,pick_totodile_json )                       // sets a cookie name as pokemon and value as pick_totodile_json
+    Cookies.set(`user_health`,`${pokemon_totodile[`health`]}` )      //sets a cookie name as user_health and goes into the object pokemon_totodile health and sets the value as health
+    location.href = `fight.html`                                     // brings you to fight page
 }
 
 
-let pokemon_chikorita = 
+let pokemon_chikorita =                                              // created variable called pokemon_chikorita that's an object, stores keys
 
     {
         name: `Chikorita`,
@@ -29,7 +29,7 @@ let pokemon_chikorita =
         attacks: `tackle`
     };
 
-    let pokemon_cyndaquil =
+    let pokemon_cyndaquil =                                         // created variable called pokemon_cyndaquil that's an object, stores keys
     {
 
         name: `Cyndaquil`,
@@ -38,7 +38,7 @@ let pokemon_chikorita =
         attacks: `tackle`
     };
 
-    let pokemon_totodile =
+    let pokemon_totodile =                                          // created variable called pokemon_totodile that's an object, stores keys
     {
 
         name: `Totadile`,
@@ -51,17 +51,17 @@ let pokemon_chikorita =
 
 
 
-let chikorita_click = document.getElementById(`chikorita_button`);
-chikorita_click.addEventListener(`click`, fight_one)
+let chikorita_click = document.getElementById(`chikorita_button`);  // gets the id and stores it as the variable chikorita_button
+chikorita_click.addEventListener(`click`, fight_one)                // when the variable chikorita_click is clicked it will start the function fight_one
 
 
-let cyndaquil_click = document.getElementById(`cyndaquil_button`);
-cyndaquil_click.addEventListener(`click`, fight_two)
+let cyndaquil_click = document.getElementById(`cyndaquil_button`);  // gets the id and stores it as the variable cyndaquil_button
+cyndaquil_click.addEventListener(`click`, fight_two)                // when the variable cyndaquil_click is clicked it will start the function fight_two
 
-let totodile_click = document.getElementById(`totadile_button`);
-totodile_click.addEventListener(`click`, fight_three)
+let totodile_click = document.getElementById(`totadile_button`);    // gets the id and stores it as the variable totadile_button
+totodile_click.addEventListener(`click`, fight_three)               // when the variable totodile_click is clicked it will start the function fight_three
 
 
-let comp_cyndaquil_json = JSON.stringify(pokemon_cyndaquil)
-Cookies.set(`comp_Pokemon`,comp_cyndaquil_json )
-Cookies.set(`comp_health`,`${pokemon_cyndaquil[`health`]}` )
+let comp_cyndaquil_json = JSON.stringify(pokemon_cyndaquil)         // creates variable (comp_cyndaquil_json) and stores comp_cyndaquil_json (object) in it using json.stringify 
+Cookies.set(`comp_Pokemon`,comp_cyndaquil_json )                    // sets a cookie name as comp_Pokemon and value as comp_cyndaquil_json
+Cookies.set(`comp_health`,`${pokemon_cyndaquil[`health`]}` )        //sets a cookie name as comp_health and goes into the object pokemon_cyndaquil and sets the value as the key health
--- js/fight.js
+++ js/fight.js
@@ -1,13 +1,13 @@
-let select_Json = Cookies.get(`Pokemon`);
-let selection_Data = JSON.parse(select_Json);
-let user_health = Cookies.get(`user_health`)
-let comp_health = Cookies.get(`comp_health`)
+let select_Json = Cookies.get(`Pokemon`);                                   // gets the cookies from Pokemon and puts it in the variable select_Json
+let selection_Data = JSON.parse(select_Json);                               // converts the string select_Json into an object and puts it as the variable selection_Data
+let user_health = Cookies.get(`user_health`)                                // gets the cookies from user_health and puts it in the variable user_health
+let comp_health = Cookies.get(`comp_health`)                                // gets the cookies from comp_health and puts it in the variable comp_health
 
 
-document.body.insertAdjacentHTML(`beforeend`,
+document.body.insertAdjacentHTML(`beforeend`,                               //targets the document body and inserts the text, takes the variable selection_Data goes and gets the key to display
 
     `<section id="comp_section">
-    <h1>${selection_Data[`name`]}</h1>
+    <h1>${selection_Data[`name`]}</h1>                                      
     <img class="pokemon_imgs"src="${selection_Data[`image_url`]}" >
     <p id="current_user_hp">${user_health}</p>
     <button id="tackle_attack">${selection_Data[`attacks`]}</button>
@@ -16,10 +16,10 @@ document.body.insertAdjacentHTML(`beforeend`,
 
 
 
-let comp_select_Json = Cookies.get(`comp_Pokemon`);
-let comp_selection_Data = JSON.parse(comp_select_Json);
+let comp_select_Json = Cookies.get(`comp_Pokemon`);                         // gets the cookie comp_Pokemon and stores it as the variable comp_select_Json
+let comp_selection_Data = JSON.parse(comp_select_Json);                     // converts the string comp_select_Json into an object and puts it as the variable comp_selection_Data
 
-document.body.insertAdjacentHTML("beforeend",
+document.body.insertAdjacentHTML("beforeend",                               //targets the document body and inserts the text, takes the variable comp_selection_Data goes and gets the key to display
 
 `<section>
 <h1>${comp_selection_Data[`name`]}</h1>
@@ -29,34 +29,34 @@ document.body.insertAdjacentHTML("beforeend",
 
 
 
-let comp_hp = document.getElementById(`current_comp_hp`);
-let user_hp = document.getElementById(`current_user_hp`);
+let comp_hp = document.getElementById(`current_comp_hp`);                   //gets the id current_comp_hp and stores it as the variable comp_hp
+let user_hp = document.getElementById(`current_user_hp`);                   //gets the id current_user_hp and stores it as the variable user_hp
 
 
-function pokemon_fight (details) {
-comp_health = comp_health -10
-Cookies.set(`comp_health`, comp_health)
-comp_hp[`innerHTML`] = (`comp_health`, comp_health)
- user_health = user_health -5
-Cookies.set(`user_health`, user_health)
-user_hp[`innerHTML`] = (`user_health`, user_health) 
+function pokemon_fight (details) {                                          
+comp_health = comp_health -10                                               // minuses 10 from the variable comp_health
+Cookies.set(`comp_health`, comp_health)                                     // sets the cookie name to comp_health and the value to the new comp_health
+comp_hp[`innerHTML`] = (`comp_health`, comp_health)                         // changes the p tag to reflect the current comp health value when an attack is clicked
+ user_health = user_health -5                                               // minuses 5 from the variable user_health
+Cookies.set(`user_health`, user_health)                                     // sets the cookie name to user_health and the value to the new user_health
+user_hp[`innerHTML`] = (`user_health`, user_health)                         // changes the p tag to reflect the current user_health value when an attack is clicked
 
-let comp_section = document.getElementById(`comp_section`)
+let comp_section = document.getElementById(`comp_section`)                  // gets the id comp_section and stores it as the variable comp_section
 
-if (comp_health <= 0) {
-    tackle_attk[`style`][`display`] = `none`
-    comp_section.insertAdjacentHTML("beforeend",`<h1>You Win</h1>`); //
- return comp_hp[`innerHTML`] = (``)
+if (comp_health <= 0) {                                                     // conditional that if the comps hp is less than or = to 0
+    tackle_attk[`style`][`display`] = `none`                                // hide the attack button 
+    comp_section.insertAdjacentHTML("beforeend",`<h1>You Win</h1>`);        // and display the h1 tag you win
+ return comp_hp[`innerHTML`] = (``)                                         // hides the comps hp so you dont see any negative numbers when u defeat it
 
    
-     }else if(user_health <= 0) {
-        tackle_attk[`style`][`display`] = `none`
-        comp_section.insertAdjacentHTML("beforeend",`<h1 class="win_lose">You Fainted</h1>`); //
-     return  user_hp[`innerHTML`] = (``) 
+     }else if(user_health <= 0) {                                                               // conditional that if the user_health is less than or = to 0
+        tackle_attk[`style`][`display`] = `none`                                                // hide the attack button 
+        comp_section.insertAdjacentHTML("beforeend",`<h1 class="win_lose">You Fainted</h1>`);   // and display the h1 tag you fainted
+     return  user_hp[`innerHTML`] = (``)                                                        // hides the user_hp so you dont see any negative numbers when u lose
 
         
     }   
 }
 
-let tackle_attk = document.getElementById(`tackle_attack`);
-tackle_attk.addEventListener(`click`, pokemon_fight)
+let tackle_attk = document.getElementById(`tackle_attack`);                 // gets the id tackle_attack and sets it as the variable tackle_attk
+tackle_attk.addEventListener(`click`, pokemon_fight)                        // when tackle_attack is clicked it will call the function pokemon fight
